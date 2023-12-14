7b07ce475b67dcf80235c0671c741e7059814c9f
gydeme <73004250+gydeme@users.noreply.github.com>
2023-12-13 21:09:51 -0600

    Update agent.py
    finalized heuristicagent

--- src/agent_files/agent.py
+++ src/agent_files/agent.py
@@ -1,4 +1,8 @@
 # from heuristics import build_settlement, place_settlement
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 def q_learning(action_values, old_state, action, reward, new_state, alpha, gamma):
     new_action = greedy(action_values[new_state])
@@ -32,9 +36,20 @@ class EpsilonGreedyPolicy:
             return POSSIBLE_ACTIONS[int(torch.argmax(q_values))]
 
 
+class QNetwork(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
 # class to initialize the heuristic agent
 class HeuristicAgent:
-    def __init__(self, policy, place_settlement_func, place_road_func, place_robber_func, choose_best_trade, place_city_func) :
+    def __init__(self, policy, place_settlement_func, place_road_func, place_robber_func, choose_best_trade, place_city_func):
         self.policy = policy
         self.place_settlement_func = place_settlement_func
         self.place_road = place_road_func
@@ -55,3 +70,16 @@ class HeuristicAgent:
     def place_road(self, game, current_player_num):
         road_coords = self.place_road(game, current_player_num)
         return road_coords
+
+    def place_robber(self, game, current_player_num):
+        robber_coords = self.place_robber(game, current_player_num)
+        return robber_coords
+
+    def choose_trade_func(self, game, current_player_num, possible_trades):
+        trade = self.choose_trade_func(game, current_player_num, possible_trades)
+        return trade
+
+    def place_city_func(self, game, valid_city_coords):
+        city_coords = self.place_city_func(game, valid_city_coords)
+        return city_coords
+
