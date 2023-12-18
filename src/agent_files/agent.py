bb2e4723b9e30100849f3fc8d782ad7e4a496615
gydeme <73004250+gydeme@users.noreply.github.com>
2023-12-18 16:05:00 -0600

    Update agent.py
    Added DQNAgent

--- src/agent_files/agent.py
+++ src/agent_files/agent.py
@@ -1,51 +1,15 @@
 # from heuristics import build_settlement, place_settlement
 import torch
-import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import random
+import numpy as np
+from agent_files.DQN import DQNNetwork, ExperienceReplay, EpsilonGreedyPolicy
+from features import Features  # Import the Features class
+from collections import namedtuple
 
-def q_learning(action_values, old_state, action, reward, new_state, alpha, gamma):
-    new_action = greedy(action_values[new_state])
-    action_values[old_state][action] += alpha * (
-            reward + gamma * action_values[new_state][new_action] - action_values[old_state][action]
-    )
-    return None
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'done'))
 
-class EpsilonGreedyPolicy:
-    def __init__(self, q_network, initial_epsilon, min_epsilon, random_state=None):
-        self.q_network = q_network
-        self.epsilon = initial_epsilon
-        self.decay_rate = (initial_epsilon - min_epsilon) / 3000
-        self.min_epsilon = min_epsilon
-        self.random_state = random_state or np.random.RandomState(seed=0)
-
-    def explore(self):
-        return self.random_state.binomial(n=1, p=self.epsilon) == 1
-
-    def decay_epsilon(self):
-        self.epsilon = max(self.min_epsilon, self.epsilon * self.decay_rate)
-
-    def __call__(self, state):
-        state_tensor = torch.from_numpy(np.array(state, dtype=np.float32))
-
-        if self.explore():
-            print("I explored!")
-            return POSSIBLE_ACTIONS[self.random_state.choice(range(self.q_network.fc2.out_features))]
-        else:
-            q_values = self.q_network(state_tensor)
-            return POSSIBLE_ACTIONS[int(torch.argmax(q_values))]
-
-
-class QNetwork(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
-        super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
 
 # class to initialize the heuristic agent
 class HeuristicAgent:
@@ -82,4 +46,98 @@ class HeuristicAgent:
     def place_city_func(self, game, valid_city_coords):
         city_coords = self.place_city_func(game, valid_city_coords)
         return city_coords
+
+
+class DQNAgent(HeuristicAgent):
+    # Initialize the DQN Agent
+    def __init__(self, input_size, output_size, place_settlement_func, place_road_func, place_robber_func,
+                 choose_best_trade, place_city_func, memory_size=500, batch_size=32, gamma=0.99, epsilon=1.0,
+                 epsilon_min=0.01, epsilon_decay=0.995, learning_rate=0.001):
+        super().__init__(self.dqn_policy, place_settlement_func, place_road_func, place_robber_func, choose_best_trade,
+                         place_city_func)
+
+        # Initialize the main model and the target model for DQN
+        self.model = DQNNetwork(input_size, output_size)
+        self.target_model = DQNNetwork(input_size, output_size)
+        self.target_model.load_state_dict(self.model.state_dict())
+
+        # Initialize experience replay for storing transitions
+        self.experience_replay = ExperienceReplay(memory_size)
+
+        # Define the epsilon-greedy policy for action selection
+        self.policy = EpsilonGreedyPolicy(self.model, epsilon, epsilon_min, epsilon_decay)
+
+        # Initialize training parameters
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+
+    # Define the policy to use for the DQN agent
+    def dqn_policy(self, game, current_player_num):
+        state = self.get_state(game, current_player_num)
+        action_number, pass_penalty = self.policy(state, game, game.players[current_player_num])
+
+        # Map the action number to the corresponding game action
+        action_mapping = {
+            0: [1, 1],
+            1: [1, 2],
+            2: [1, 3],
+            3: [1, 4],
+            4: [2, None],
+            5: [3, None],
+            6: [4, None]
+        }
+
+        return action_mapping.get(action_number, [4, None]), pass_penalty  # Default action if action_number is not in the mapping
+
+    # Get the current state representation for the DQN input
+    def get_state(self, game, current_player_num):
+        features = Features(game, game.players[current_player_num])
+        state = features.flattenFeature(game, game.players[current_player_num])
+        return np.array(state, dtype=np.float32)
+
+    # Training step for the DQN agent
+    def train(self):
+        # Check if the memory is sufficient for training
+        if len(self.experience_replay.memory) < self.batch_size:
+            return
+        transitions = self.experience_replay.sample(self.batch_size)
+        batch = Transition(*zip(*transitions))
+
+        # Convert each state, action, and reward in the batch to a Tensor
+        state_batch = torch.stack([torch.tensor(s, dtype=torch.float32) for s in batch.state])
+        action_batch = torch.tensor([a for a in batch.action], dtype=torch.long)
+        action_batch = action_batch.unsqueeze(1)
+        reward_batch = torch.tensor([r for r in batch.reward], dtype=torch.float32)
+
+        # Create a mask of non-final states
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool)
+        non_final_next_states = torch.stack(
+            [torch.tensor(s, dtype=torch.float32) for s in batch.next_state if s is not None])
+
+        # Compute current Q values
+        state_action_values = self.model(state_batch).gather(1, action_batch)
+
+        # Compute next Q values from the target network for non-final states
+        next_state_values = torch.zeros(self.batch_size)
+        next_state_values[non_final_mask] = self.target_model(non_final_next_states).max(1)[0].detach()
+
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute the loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Update epsilon for the epsilon-greedy policy
+        self.policy.decay_epsilon()
+
+    # Update the target network with weights from the main model
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
 
