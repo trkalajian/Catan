643cf40786e020fb0f0cf446073a9848b8119248
gydeme <73004250+gydeme@users.noreply.github.com>
2023-12-18 15:57:49 -0600

    Update train.py
    Added all required changes for DQN agent to run and learn alongside Actor Critic

--- src/train.py
+++ src/train.py
@@ -1,24 +1,26 @@
 from pycatan import Game, DevelopmentCard, Resource
 from pycatan.board import BeginnerBoard, BoardRenderer, BuildingType
-from methods import choose_path, choose_intersection, choose_resource, move_robber, count_cards, resource_check, choose_hex, get_coord_sort_by_xy
+from methods import choose_path, choose_intersection, choose_resource, move_robber, count_cards, resource_check, \
+    choose_hex, get_coord_sort_by_xy
 from agent_files.heuristics import heuristic_policy
-from agent_files.agent import HeuristicAgent, EpsilonGreedyPolicy, DQNAgent
-from agent_files.heuristics import build_settlement, place_settlement, heuristic_policy, choose_road_placement, place_robber, choose_best_trade, place_city
+from agent_files.agent import HeuristicAgent, DQNAgent, EpsilonGreedyPolicy
+from agent_files.heuristics import build_settlement, place_settlement, heuristic_policy, choose_road_placement, \
+    place_robber, choose_best_trade, place_city
 import actorcritic
 import random
 import pickle
 import os
-import pandas as pd
+import features
+from features import Features
 import numpy as np
 from actorcritic import ActorCritic
 import matplotlib.pyplot as plt
 
-
 # set number of players here
 numAgents = 4
-num_iterations = 100
-num_episodes = 100
-save_every = 2
+num_iterations = 10
+num_episodes = 250
+save_every = 100
 all_rewards = np.zeros((num_iterations, num_episodes, numAgents))
 final_vps = np.zeros((num_iterations, num_episodes, numAgents))
 winsPerPlayer = np.zeros(numAgents)
@@ -32,6 +34,20 @@ def create_new_game():
     game = Game(board, numAgents)
     renderer = BoardRenderer(game.board, {})
     return game, renderer
+
+def translate_action_to_index(action_pair):
+    action_mapping = {
+        (1, 1): 0,
+        (1, 2): 1,
+        (1, 3): 2,
+        (1, 4): 3,
+        (2, None): 4,
+        (3, None): 5,
+        (4, None): 6
+    }
+    action_pair_tuple = tuple(action_pair)  # Convert the action pair list to a tuple
+    return action_mapping.get(action_pair_tuple, 6)  # Default to 'PASS_TURN' if action_pair not in mapping
+
 
 def save_progress(agents, all_rewards, final_vps, episode, iteration):
     """
@@ -47,15 +63,15 @@ def save_progress(agents, all_rewards, final_vps, episode, iteration):
     for i, agent in enumerate(agents):
         if type(agent) is not HeuristicAgent:
             policy_type = 'theta' if hasattr(agent, 'theta') else 'dqn'
-            policy_filename = f'results/{policy_type}/{policy_type}_policy_{episode+1}_{iteration+1}.pkl'
+            policy_filename = f'results/{policy_type}/{policy_type}_policy_{episode + 1}_{iteration + 1}.pkl'
             with open(policy_filename, 'wb') as f:
                 policy_data = agent.theta if hasattr(agent, 'theta') else agent.model.state_dict()
                 pickle.dump(policy_data, f)
-            print(f"Saved policy for Player {i} ({policy_type}) at episode {episode+1}, iteration {iteration+1}")
+            print(f"Saved policy for Player {i} ({policy_type}) at episode {episode + 1}, iteration {iteration + 1}")
 
-    np.savetxt(f'results/rewards/{iteration}iteration_rewards_{episode+1}.csv', all_rewards[iteration], delimiter=',')
-    np.savetxt(f'results/final_vps/{iteration}iteration_vps_{episode+1}.csv', final_vps[iteration], delimiter=',')
-    print(f"Rewards and victory points saved at episode {episode+1}, iteration {iteration+1}")
+    np.savetxt(f'results/rewards/{iteration}iteration_rewards_{episode + 1}.csv', all_rewards[iteration], delimiter=',')
+    np.savetxt(f'results/final_vps/{iteration}iteration_vps_{episode + 1}.csv', final_vps[iteration], delimiter=',')
+    print(f"Rewards and victory points saved at episode {episode + 1}, iteration {iteration + 1}")
 
 
 # function to calculate the reward from the most recent distribution of resources
@@ -89,10 +105,11 @@ def heuristic_agent_maker():
         place_city_func=place_city
     )
 
+
 # method to build an agent that runs DQN. Contains all hyperparameters and heuristics
 def DQN_agent_maker():
-    return DQNAgent(input_size=22,
-                    output_size=7,
+    return DQNAgent(input_size=44,  # number of features in features.py after flattening
+                    output_size=7,  # number of available actions
                     place_settlement_func=place_settlement,
                     place_road_func=choose_road_placement,
                     place_robber_func=place_robber,
@@ -103,31 +120,26 @@ def DQN_agent_maker():
                     gamma=0.99,
                     epsilon=0.9,
                     epsilon_min=0.01,
-                    epsilon_decay=0.995,
-                    learning_rate=0.001
+                    epsilon_decay=0.99,
+                    learning_rate=0.01
                     )
+
 
 # method to implement actor critic
 def actor_critic_maker():
     return actorcritic.ActorCritic(place_settlement_func=place_settlement,
-                                    place_road_func=choose_road_placement,
-                                    place_robber_func=place_robber,
-                                    choose_best_trade=choose_best_trade,
-                                    place_city_func=place_city
+                                   place_road_func=choose_road_placement,
+                                   place_robber_func=place_robber,
+                                   choose_best_trade=choose_best_trade,
+                                   place_city_func=place_city
                                    )
 
-# for i in range(numAgents):
-#     # change some of these to actor-critics when appropriate
-#     if i >= 1:
-#         agents.append(heuristic_agent_maker())
-#     else:
-#         # creates an actor critic
-#         agents.append(actor_critic_maker())
-#
+
 # # Main game loop
 current_player_num = 0
 is_start = False
 num_games = 0
+num_turns = 0
 for iteration in range(num_iterations):
     agents = []
     for j in range(numAgents):
@@ -137,6 +149,7 @@ for iteration in range(num_iterations):
         else:
             # creates an actor critic
             agents.append(actor_critic_maker())
+            # agents.append(DQN_agent_maker())
 
     # Main game loop
     for ep in range(num_episodes):
@@ -144,6 +157,7 @@ for iteration in range(num_iterations):
         total_reward = np.zeros(numAgents)  # Initialize total reward for this episode
         # Create a new board and game for each iteration to reset the board
         game, renderer = create_new_game()
+        choices = []
         # Creating players and setting the order
         player_order = list(range(len(game.players)))
         random.shuffle(player_order)
@@ -160,26 +174,32 @@ for iteration in range(num_iterations):
                                   ensure_connected=False)
             current_player.add_resources(game.board.get_hex_resources_for_intersection(agent_coords))
             # road_options = game.board.get_valid_road_coords(current_player, connected_intersection=agent_coords)
-            road_coords = agents[i].place_road(game, i)
+            road_coords = agents[i].place_road(game, i, is_start)
             game.build_road(player=current_player, path_coords=road_coords, cost_resources=False)
 
         print(game.board)
-        current_player_num = 0
-        is_start = False
+        current_player_num = player_order[0]
+        dice = 0
         while True:
+            num_turns += 1
             current_player = game.players[current_player_num]
+            current_feats = Features(game, current_player)
+            current_state = current_feats.flattenFeature(game, game.players[current_player_num])  # get the current state before action
+            turn_reward = 0
             # Roll the dice
             dice = random.randint(1, 6) + random.randint(1, 6)
+            is_start = False
             if dice == 7:
                 card_totals = count_cards(game)
                 sev_reward = resource_check(card_totals,
                                             game)  # discards people over 7 and returns neg reward porportional to discard
+                turn_reward += sev_reward[0]
                 for i in range(numAgents):
                     total_reward[i] += sev_reward[i]
                 hex_coords, player_stolen = agents[current_player_num].place_robber(game, current_player_num)
                 move_robber(current_player, game, hex_coords, player_stolen)
                 pass
-            else:  # give our resources and rewards
+            else:  # give out resources and rewards
                 previous_total_resources = np.zeros(numAgents)
                 for i in range(numAgents):
                     cur_player = game.players[i]
@@ -189,13 +209,21 @@ for iteration in range(num_iterations):
                 game.add_yield_for_roll(dice)  # add the yield for the dice roll
 
                 resource_rewards = resource_reward(game, previous_total_resources)  # calculate each players reward
+                # turn_reward += resource_rewards[0]
                 for i in range(numAgents):
                     total_reward[i] += resource_rewards[i]
             choice = [0, 0]
             while choice[0] != 4:
                 while True:
                     try:
-                        choice = agents[current_player_num].policy(game, current_player_num)
+                        if type(agents[current_player_num]) is DQNAgent:
+                            choice, pass_penalty = agents[current_player_num].dqn_policy(game, current_player_num)
+                            num_choice = translate_action_to_index(choice)
+                            if pass_penalty:
+                                turn_reward += -1   # penalizes the player if they passed with available actions
+                            choices.append(choice)
+                        else:
+                            choice = agents[current_player_num].policy(game, current_player_num)
                         print(choice)
                         if 1 <= choice[0] <= 4:
                             break  # Valid choice, exit the loop
@@ -214,6 +242,7 @@ for iteration in range(num_iterations):
                         print("Player %d is building a settlement" % (current_player_num + 1))
                         coords = agents[current_player_num].place_settlement(game, current_player_num, is_start)
                         game.build_settlement(current_player, coords)
+                        turn_reward += 5  # turn reward for the VP earned from settlement
                     elif building_choice == 2:
                         if not current_player.has_resources(BuildingType.CITY.get_required_resources()):
                             continue
@@ -222,13 +251,14 @@ for iteration in range(num_iterations):
                         coords = agents[current_player_num].place_city_func(game, valid_coords)
                         print("Player %d is building a city" % (current_player_num + 1))
                         game.upgrade_settlement_to_city(current_player, coords)
+                        turn_reward += 5  # turn reward for the VP earned from the city
                     elif building_choice == 3:
                         if not current_player.has_resources(BuildingType.ROAD.get_required_resources()):
                             continue
                         valid_coords = game.board.get_valid_road_coords(current_player)
                         if not valid_coords:
                             continue
-                        path_coords = agents[current_player_num].place_road(game, current_player_num)
+                        path_coords = agents[current_player_num].place_road(game, current_player_num, is_start)
                         print("Player %d is building a road" % (current_player_num + 1))
                         game.build_road(current_player, path_coords)
                     elif building_choice == 4:
@@ -277,18 +307,9 @@ for iteration in range(num_iterations):
                 winsPerPlayer[current_player_num] += 1
                 # Calculate final VP rewards for all players
                 vp_rewards = final_vp_reward(game)
+                turn_reward += vp_rewards[0]  # final rewards for the round
                 for i in range(numAgents):
                     total_reward[i] += vp_rewards[i]
-
-                # if isinstance(agents[current_player_num], ActorCritic):
-                #     # This needs to pass in a reward that includes winning the game, put in 100 for now
-                #     total_reward += 100
-                #     agents[current_player_num].terminateEpisode(100)
-                # for i in range(len(agents)):
-                #     if i != current_player_num and isinstance(agents[i], ActorCritic):
-                #         # rewards players who didnt win based on their VP number
-                #         total_reward = game.get_victory_points(game.players[i])
-                #         agents[i].terminateEpisode(game.get_victory_points(game.players[i]))
 
                 print("Current Victory point standings:")
                 for i in range(len(game.players)):
@@ -321,16 +342,30 @@ for iteration in range(num_iterations):
                         # agents[i].terminateEpisode(all_rewards[iteration][ep][i])
                         agents[i].terminateEpisode(0)
                     if isinstance(agents[i], ActorCritic) and i == current_player_num:
-                        # print("Reward: " + str(all_rewards[iteration][ep][i]))
-                        # agents[i].terminateEpisode(all_rewards[iteration][ep][i])
                         agents[i].terminateEpisode(100)
+                # train the DQN agent
+                for agent in agents:
+                    if isinstance(agent, DQNAgent):
+                        next_feats = Features(game, current_player)
+                        next_state = next_feats.flattenFeature(game, game.players[current_player_num])
+                        agent.experience_replay.push(current_state, num_choice, next_state,
+                                                     turn_reward, done=True)
+                        agent.train()
+                        print(choices)
                 # saves the policy, rewards, and final VPs every save_every episodes
                 if ep % save_every == 0:
                     save_progress(agents, all_rewards, final_vps, ep, iteration)
                 break
 
-            current_player_num = (current_player_num + 1) % len(game.players)
+            if isinstance(agents[current_player_num], DQNAgent) and not is_start:
+                next_feats = Features(game, current_player)
+                next_state = next_feats.flattenFeature(game, game.players[current_player_num])
+                agents[current_player_num].experience_replay.push(current_state, num_choice, next_state, turn_reward,
+                                                              done=False)
+                if num_turns % 80 == 0:
+                    agents[current_player_num].train()
 
+            current_player_num = (current_player_num + 1) % len(game.players)
 
 # the final policy is stored here
 # actorCriticTrainedPolicy = []
